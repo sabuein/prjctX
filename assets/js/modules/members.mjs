cfc357248242a09658d9b917b5fe3a850ddd75c4
Salaheddin AbuEin <sabuein@gmail.com>
2023-03-23 03:58:58 +0000

    Separting view from some logic

--- assets/js/modules/members.mjs
+++ assets/js/modules/members.mjs
@@ -1,88 +1,71 @@
-async function loadMembers(request) {
-  let headersList = {
-    "Accept": "*/*",
-    "User-Agent": "prjctX (https://github.com/sabuein/prjctX)",
-    "Accept-Charset": "utf-8",
-    "Connection": "keep-alive",
-  }
+import { responseError } from "./helpers.mjs";
 
-  return await (await fetch(request, {
-    method: "get",
-    mode: "cors",
-    headers: headersList
-  }).catch(handleError)).text();
+const loadMembers = async (request) => {
+  try {
+    let headersList = {
+      "Accept": "*/*",
+      "User-Agent": "prjctX (https://github.com/sabuein/prjctX)",
+      "Accept-Charset": "utf-8",
+      "Connection": "keep-alive",
+    }
+
+    return await (await fetch(request, {
+      method: "get",
+      mode: "cors",
+      headers: headersList
+    })).text();
+
+  } catch (error) { responseError(error); }
 }
 
 const addMember = async (data) => {
-  const request = new Request("http://localhost:8888/collectors/add");
-  let headersList = {
-    "Accept": "*/*",
-    "User-Agent": "prjctX (https://github.com/sabuein/prjctX)",
-    "Accept-Charset": "utf-8",
-    "Connection": "keep-alive",
-    "Content-Type": "application/json"
-  }
-  return await (await fetch(request, {
-    method: "post",
-    mode: "no-cors",
-    headers: headersList,
-    body: JSON.stringify(data)
-  }).catch(handleError)).text();
+  try {
+    let request = new Request("http://localhost:8888/collectors/add");
+
+    let headersList = {
+      "Accept": "*/*",
+      "User-Agent": "prjctX (https://github.com/sabuein/prjctX)",
+      "Accept-Charset": "utf-8",
+      "Connection": "keep-alive",
+      "Content-Type": "application/json"
+    }
+
+    return await (await fetch(request, {
+      method: "post",
+      mode: "no-cors",
+      headers: headersList,
+      body: JSON.stringify(data)
+    })).text();
+
+  } catch (error) { responseError(error); }
 }
 
-async function updateMember(request, id) {
-  let headersList = {
-    "Accept": "*/*",
-    "User-Agent": "prjctX (https://github.com/sabuein/prjctX)",
-    "Accept-Charset": "utf-8",
-    "Connection": "keep-alive",
-    "Content-Type": "application/json"
-  }
+const updateMember = async (request, id) => {
+  try {
+    let headersList = {
+      "Accept": "*/*",
+      "User-Agent": "prjctX (https://github.com/sabuein/prjctX)",
+      "Accept-Charset": "utf-8",
+      "Connection": "keep-alive",
+      "Content-Type": "application/json"
+    }
 
-  let bodyContent = JSON.stringify({
-    "name": "Zainab Al-Abdulrahman",
-    "email": "zainab.alabdulrahman@example.com",
-    "phone": "+966561234567"
-  });
+    let bodyContent = JSON.stringify({
+      "name": "Zainab Al-Abdulrahman",
+      "email": "zainab.alabdulrahman@example.com",
+      "phone": "+966561234567"
+    });
 
-  request.url = `${request.url}${id}`
+    request.url = `${request.url}${id}`
 
-  return await (await fetch(request, {
-    method: "put",
-    mode: "cors",
-    headers: headersList,
-    body: bodyContent
-  }).catch(handleError)).text();
+    return await (await fetch(request, {
+      method: "put",
+      mode: "cors",
+      headers: headersList,
+      body: bodyContent
+    })).text();
+
+  } catch (error) { responseError(error); }
 }
 
-const handleError = (error) => {
-  console.warn(error);
-  return new Response(
-    JSON.stringify({
-      code: 400,
-      message: "There has been a problem with fetch operation"
-    })
-  );
-}
-
-const renderMembers = (input, output) => {
-  const members = JSON.parse(input);
-  for (const member of members) {
-    const row = document.createElement("tr");
-    row.appendChild(document.createElement("th")).textContent = member.id;
-    row.appendChild(document.createElement("td")).textContent = member.name;
-    row.appendChild(document.createElement("td")).innerHTML = `<a href="mailto:${member.email}">${member.email}</a>`;
-    row.appendChild(document.createElement("td")).innerHTML = `<a href="tel:${member.phone}">${member.phone}</a>`;
-    row.firstElementChild.setAttribute("scope", "row");
-    output.appendChild(row);
-  }
-  const row = document.createElement("tr");
-  const final = document.createElement("td");
-  final.setAttribute("colspan", 4);
-  final.setAttribute("scope", "row");
-  final.textContent = `Our community is made up of ${output.childElementCount} members — and counting...`;
-  row.appendChild(final);
-  output.appendChild(row);
-}
-
-export { loadMembers, addMember, updateMember, renderMembers };
+export { loadMembers, addMember, updateMember };
---
+++ assets/js/modules/view.mjs
@@ -0,0 +1,43 @@
+import { cl } from "./helpers.mjs";
+
+const renderMembers = (json, table) => {
+    try {
+        const members = JSON.parse(json);
+        for (const member of members) {
+            const row = document.createElement("tr");
+            row.appendChild(document.createElement("th")).textContent = member.id;
+            row.appendChild(document.createElement("td")).textContent = member.name;
+            row.appendChild(document.createElement("td")).innerHTML = `<a href="mailto:${member.email}">${member.email}</a>`;
+            row.appendChild(document.createElement("td")).innerHTML = `<a href="tel:${member.phone}">${member.phone}</a>`;
+            row.firstElementChild.setAttribute("scope", "row");
+            table.insertBefore(row, table.firstChild);
+        }
+        const row = document.createElement("tr");
+        const final = document.createElement("td");
+        final.setAttribute("colspan", 4);
+        final.setAttribute("scope", "row");
+        final.textContent = `Our community is made up of ${table.childElementCount} members — and counting...`;
+        row.appendChild(final);
+        table.appendChild(row);
+
+    } catch (error) { table.innerHTML = `<pre>${error}</pre>`; }
+}
+
+const setCustomComponent = (className, elementName) => {
+    if (!window.customElements.get(elementName)) {
+        window.customElements.define(elementName, className);
+    }
+};
+
+const prettyJson = (object) => JSON.stringify(object, null, "\t");
+
+const handlebars = (source, data) => {
+    try {
+        const template = Handlebars.compile(source.innerHTML);
+        source.innerHTML = template(data);
+    } catch (reason) {
+        console.warn(`Handlebars.js didn't load: ${reason}`);
+    }
+}
+
+export { renderMembers, setCustomComponent, prettyJson as pJson, handlebars };
